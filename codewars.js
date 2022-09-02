294504d434a1a9a51a34f2dff945680550a8e3c3
Nadufa <nadufa18404@gmail.com>
2022-09-02 12:11:46 +0300

    first commit

---
+++ codewars.js
@@ -0,0 +1,53 @@
+//TASK
+
+// const beast = "cat"
+// const dish = "milk"
+
+// function feast(a, b) {
+//     console.log("beast = " + beast)
+//     console.log("dish = " + dish)
+
+//     const arr1 = beast.split('');
+//     const arr2 = dish.split('');
+//     console.log("arr1 = " + arr1)
+//     console.log("arr2 = " + arr2)
+
+//     const firstLetter1 = arr1[0]
+//     const firstLetter2 = arr2[0]
+//     console.log("firstLetter1 = " + firstLetter1)
+//     console.log("firstLetter2 = " + firstLetter2)
+
+//     console.log("arr1.lenght = " + arr1.length)
+//     console.log("arr2.lenght = " + arr2.length)
+
+//     const lastLetter1 = arr1[arr1.length - 1];
+//     const lastLetter2 = arr2[arr2.length - 1];
+//     console.log("lastLetter1 = " + lastLetter1)
+//     console.log("lastLetter2 = " + lastLetter2)
+
+//     if (firstLetter1 == firstLetter2 && lastLetter1 == lastLetter2) {
+//         console.log("true");
+//     } else {
+//         console.log("false");
+//     }
+// }
+
+// feast(beast, dish)
+
+//TASK
+
+
+
+//TASK
+
+
+
+//TASK
+
+
+
+//TASK
+
+
+
+//TASK
